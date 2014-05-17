c23c3bb4548897661cfdf5b5c146336c7c4621b6
ray <ray@dagon>
2014-05-17 21:31:17 +0100

    Can add orders from js gui

--- starbucks/barrista/static/barrista/js/barrista.js
+++ starbucks/barrista/static/barrista/js/barrista.js
@@ -9,6 +9,16 @@ barrista.OrderGui = function(order_div)
 barrista.OrderGui.prototype.show = function()
 {
     this.orders_div.empty();
+    this.customer_orders = $("<div>", {"id": "customer_orders"});
+    this.order_form = $("<div>", {"id": "order_form"});
+    this.orders_div.append(
+        $("<div>", {"id": "leftcolumn"}).append(
+            this.customer_orders
+        ),
+        $("<div>", {"id": "rightcolumn"}).append(
+            this.order_form
+        )
+    );
     this.load_products();
 }
 
@@ -18,6 +28,7 @@ barrista.OrderGui.prototype.load_products = function()
     network.get("/products", function(data){
         self.products_loaded(data);
         self.load_orders();
+        self.show_order_form();
     });
 }
 
@@ -39,16 +50,56 @@ barrista.OrderGui.prototype.orders_loaded = function(data)
     for (var o in data)
     {
         var order = data[o];
-        this.orders_div.append(
+        this.customer_orders.append(
             $("<div>", {"class": "order", "text": this.products[order.product_id] + " for " + order.customer_name})
         );
     }
 }
 
+barrista.OrderGui.prototype.show_order_form = function()
+{
+    this.order_form.append(
+        $("<div>", {"id": "order_form"}).append(
+            $("<div>", {"class": "field"}).append(
+                $("<div>", {"class": "name", "text": "Product"}),
+                this.build_products_select()
+            ),
+            $("<div>", {"class": "field"}).append(
+                $("<div>", {"class": "name", "text": "Customer"}),
+                $("<input>", {"class": "customer"})
+            ),
+            $("<div>", {"class": "field"}).append(
+                $("<button>", {"class": "submit_order", "text": "Submit"}).click(this.submit_order)
+            )
+        )
+    );
+}
+
+barrista.OrderGui.prototype.submit_order = function()
+{
+    var product_id = $("#order_form .products").find(":selected").val();
+    var customer_name = $("#order_form .customer").val();
+    console.log("Adding order: " + product_id + " " + customer_name);
+    network.post("/orders", {"product_id": product_id, "customer_name": customer_name});
+}
+
+barrista.OrderGui.prototype.build_products_select = function()
+{
+    var products_select = $("<select>", {"class": "products"});
+    for (var product_id in this.products)
+    {
+        var product_name = this.products[product_id];
+        products_select.append(
+            $("<option>", {"class": "product", "value": product_id, "text": product_name})
+        );
+    }
+    return products_select;
+}
+
 function init()
 {
     console.log("Init");
-    gui = new barrista.OrderGui($("#orders"))
+    gui = new barrista.OrderGui($("#main"))
     gui.show();
 }
 
--- starbucks/barrista/static/barrista/js/network.js
+++ starbucks/barrista/static/barrista/js/network.js
@@ -17,3 +17,22 @@ network.get = function(url, callback, onerror)
         'type': 'GET'
     });
 }
+
+network.post = function(url, data, callback, onerror)
+{
+    $.ajax({
+        'url': url,
+        'data': data,
+        'success': function(data) {
+            if (callback != null)
+                callback(data);
+        },
+        'error': function(jqXHR, errorText) {
+            console.log("Error calling "+url+" : "+errorText);
+            if (onerror)
+                onerror(errorText, jqXHR);
+        },
+        'type': 'POST'
+    });
+}
+
